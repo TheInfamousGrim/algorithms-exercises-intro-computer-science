93963d82fda2b1c2f17b7e4a90d9085196064e85
George Fincher <george@grimfunky.dev>
2023-10-08 18:58:52 +0100

    Feat ✨: complete quick sort algorithm

--- specs/quick-sort/quick-sort.test.js
+++ specs/quick-sort/quick-sort.test.js
@@ -14,11 +14,36 @@
 
 function quickSort(nums) {
   // code goes here
+
+  // Base case
+  if (nums.length <= 1) return nums;
+
+  // Find the pivot
+  const pivot = nums[nums.length - 1];
+
+  // Create the left and right arrays
+  const left = [];
+  const right = [];
+
+  // Loop through the array
+  for (let i = 0; i < nums.length - 1; i++) {
+    // If the current number is less than the pivot
+    if (nums[i] < pivot) {
+      // Add the smaller number to the end of the left array
+      left.push(nums[i]);
+    } else {
+      // Add the larger number to the end of the right array
+      right.push(nums[i]);
+    }
+  }
+
+  // Recursively sort the left and right arrays
+  return [...quickSort(left), pivot, ...quickSort(right)];
 }
 
 // unit tests
 // do not modify the below code
-test.skip("quickSort", function () {
+test("quickSort", function () {
   const input = [10, 8, 2, 1, 6, 3, 9, 4, 7, 5];
   const answer = quickSort(input);
 
